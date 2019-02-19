344060b12c696f29290a8db97f94dfe4fb3048b4
Bastien Nocera <hadess@hadess.net>
2019-02-19 19:59:11 +0100

    build: Fix builddir != srcdir

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,16 @@
 #! /bin/sh
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+ORIGDIR=`pwd`
+
+cd $srcdir
+
 gtkdocize || exit 1
 autoreconf -v --install || exit 1
+cd $ORIGDIR || exit $?
+
 if test -z "$NOCONFIGURE"; then
-    ./configure "$@" || exit 1
+    $srcdir/configure "$@" || exit 1
 fi
